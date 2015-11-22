88d34d4a04625b02fdd9774e6a4c7bc5515e211c
xz42 <xz42@112eb098-312c-4e5a-b4cf-225ce5e3927e>
2015-11-22 06:52:09 +0000

    add game MVC

--- src/xz42_bb26/game/controller/GameController.java
+++ src/xz42_bb26/game/controller/GameController.java
@@ -1,5 +1,30 @@
 package xz42_bb26.game.controller;
 
-public class GameController {
+import common.IChatroom;
+import xz42_bb26.game.model.GameModel;
+import xz42_bb26.game.view.GameView;
+import xz42_bb26.game.view.IModelAdapter;
 
+public class GameController {
+	/**
+	 * Model of the MVC.
+	 */
+	private GameModel model;
+	/**
+	 * View of the MVC
+	 */
+	private GameView view;
+	
+	/**
+	 * Constructor of the controller.
+	 * @param team The team.
+	 */
+	public GameController() {
+		model = new GameModel(new IViewAdapter() {
+			
+		});
+		view = new GameView(new IModelAdapter() {
+			
+		});
+	}
 }
